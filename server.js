051aae3d06a9b8520f4ee00b98d38073af69c4ae
caisepolente <steve.durighello@gmail.com>
2016-06-29 23:45:30 +0100

    cluster for multicore servers

--- server.js
+++ server.js
@@ -5,7 +5,12 @@
 var init = require('./config/init')(),
 	config = require('./config/config'),
 	mongoose = require('mongoose'),
+    _ = require('lodash'),
 	chalk = require('chalk');
+
+// Dependencies for Cluster
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
 
 /**
  * Main application entry file.
@@ -26,8 +31,52 @@ var app = require('./config/express')(db);
 // Bootstrap passport config
 require('./config/passport')();
 
-// Start the app by listening on <port>
-app.listen(config.port);
+// // Start the app by listening on <port>
+// app.listen(config.port);
+
+// Start the app by listening on <port> after forking cluster
+
+var forkWorkers = function(){
+    var worker = cluster.fork();
+    worker.on('online', function(){
+        console.log('Cluster worker ' + worker.id + ' is online');
+    });
+};
+
+if (cluster.isMaster) {
+    // Fork workers.
+    for (var i = 0; i < numCPUs; i++) {
+        forkWorkers();
+    }
+    // Restarting workers that crashed
+    cluster.on('exit', function(worker, code){
+        if(code !== 0 && !worker.suicide){
+            console.log('worker '+ worker.id +' crashed. Starting a new worker');
+            cluster.fork();
+        }
+    });
+    // Zero downtime restart
+    process.on('SIGUSR2', function(){
+        console.log('Restarting workers');
+        var workers = Object.keys(cluster.workers);
+        function restartWorker(i){
+            if(i >= workers.length) return;
+            var worker = cluster.workers[workers[i]];
+            worker.disconnect();
+
+            worker.on('exit', function(){
+                if(!worker.suicide) return;
+                var newWorker = cluster.fork();
+                newWorker.on('listening', function(){
+                    restartWorker(i+1);
+                });
+            });
+        }
+        restartWorker(0);
+    });
+} else {
+    app.listen(config.port);
+}
 
 // Expose app
 exports = module.exports = app;
