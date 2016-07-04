c970b90bc2bcf2d0809f36e258b021dac8d73ce9
caisepolente <steve.durighello@gmail.com>
2016-07-04 16:20:59 +0200

    cluster for multicore servers

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var init = require('./config/init')(),
 
 // Dependencies for Cluster
 var cluster = require('cluster');
-var numCPUs = require('os').cpus().length;
+var os = require('os');
 
 /**
  * Main application entry file.
@@ -36,7 +36,7 @@ require('./config/passport')();
 
 // Start the app by listening on <port> after forking cluster
 
-var forkWorkers = function(){
+var forkWorker = function(){
     var worker = cluster.fork();
     worker.on('online', function(){
         console.log('Cluster worker ' + worker.id + ' is online');
@@ -44,36 +44,43 @@ var forkWorkers = function(){
 };
 
 if (cluster.isMaster) {
-    // Fork workers.
-    for (var i = 0; i < numCPUs; i++) {
-        forkWorkers();
-    }
-    // Restarting workers that crashed
-    cluster.on('exit', function(worker, code){
-        if(code !== 0 && !worker.suicide){
-            console.log('worker '+ worker.id +' crashed. Starting a new worker');
-            cluster.fork();
-        }
-    });
-    // Zero downtime restart
-    process.on('SIGUSR2', function(){
-        console.log('Restarting workers');
-        var workers = Object.keys(cluster.workers);
-        function restartWorker(i){
-            if(i >= workers.length) return;
-            var worker = cluster.workers[workers[i]];
-            worker.disconnect();
 
-            worker.on('exit', function(){
-                if(!worker.suicide) return;
-                var newWorker = cluster.fork();
-                newWorker.on('listening', function(){
-                    restartWorker(i+1);
-                });
-            });
+    var cpus = os.cpus().length;
+    for (var i = 0; i < cpus; i++) {
+        forkWorker();
+    }
+
+    cluster.on('exit', function(worker, code) {
+        if(code !== 0 && !worker.suicide) {
+            console.log('Worker crashed. Starting a new worker');
+            forkWorker();
         }
-        restartWorker(0);
     });
+
+    // process.on('SIGUSR2', function() {
+    //     console.log('Restarting workers');
+    //     var workers = Object.keys(cluster.workers);
+    //
+    //     function restartWorker(i) {
+    //         if(i >= workers.length) return;
+    //         var worker = cluster.workers[workers[i]];
+    //         console.log(i);
+    //         console.log('Stopping worker '+ i +': ' + worker.process.pid);
+    //         worker.disconnect();
+    //
+    //         worker.on('exit', function() {
+    //             if(!worker.suicide) return;
+    //             var newWorker = cluster.fork();
+    //             newWorker.on('listening', function() {
+    //                 restartWorker(i + 1);
+    //             });
+    //         });
+    //     }
+    //
+    //     restartWorker(0);
+    //
+    // });
+
 } else {
     app.listen(config.port);
 }
@@ -82,4 +89,4 @@ if (cluster.isMaster) {
 exports = module.exports = app;
 
 // Logging initialization
-console.log('MEAN.JS application started on port ' + config.port);
+console.log('Application started on port ' + config.port);
