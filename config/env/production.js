c3749b151d9d8b0735816c5e224817cf9cd7ccaf
caisepolente <steve.durighello@gmail.com>
2016-07-04 18:00:56 +0200

    process.env.mongolab_uri -> mongodb_uri

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/meanjs-test2',
+	db: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/meanjs-test2',
 	assets: {
 		lib: {
 			css: [
